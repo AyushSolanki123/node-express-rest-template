b9d4d0e63b37b6de9a95619cde77df0f3f780742
Ayush Solanki <ayush.solanki1981@gmail.com>
2023-03-01 10:23:02 +0530

    env changes

--- Utils/Helper.js
+++ Utils/Helper.js
@@ -1,41 +1,40 @@
 const jwt = require("jsonwebtoken");
-const { env } = require("../env");
 
 function generateAuthPairs(payload, refreshPayload) {
-	return new Promise((resolve, reject) => {
-		const jwtKey = env.jwtKey;
-		const refreshKey = env.refreshKey;
-		const authToken = jwt.sign(payload, jwtKey, {
-			expiresIn: env.tokenValidity,
-		});
-		const refreshToken = jwt.sign(refreshPayload, refreshKey);
-		resolve({
-			authToken: authToken,
-			refreshToken: refreshToken,
-		});
-	});
+    return new Promise((resolve, reject) => {
+        const jwtKey = process.env.jwtKey;
+        const refreshKey = process.env.refreshKey;
+        const authToken = jwt.sign(payload, jwtKey, {
+            expiresIn: process.env.tokenValidity,
+        });
+        const refreshToken = jwt.sign(refreshPayload, refreshKey);
+        resolve({
+            authToken: authToken,
+            refreshToken: refreshToken,
+        });
+    });
 }
 
 function validateAuthToken(token, tokenType = 0) {
-	// 0 for auth token , 1 for refresh token
-	return new Promise(function (resolve, reject) {
-		const jwtKey = env.jwtKey;
-		const refreshKey = env.refreshKey;
-		jwt.verify(
-			token,
-			tokenType === 0 ? jwtKey : refreshKey,
-			(err, decoded) => {
-				if (err) {
-					reject(err);
-				} else {
-					resolve(decoded);
-				}
-			}
-		);
-	});
+    // 0 for auth token , 1 for refresh token
+    return new Promise(function (resolve, reject) {
+        const jwtKey = process.env.jwtKey;
+        const refreshKey = process.env.refreshKey;
+        jwt.verify(
+            token,
+            tokenType === 0 ? jwtKey : refreshKey,
+            (err, decoded) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(decoded);
+                }
+            }
+        );
+    });
 }
 
 module.exports = {
-	generateAuthPairs: generateAuthPairs,
-	validateAuthToken: validateAuthToken,
+    generateAuthPairs: generateAuthPairs,
+    validateAuthToken: validateAuthToken,
 };
